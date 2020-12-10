28781a6564a92a3abe936eb8ddb0599f912ae985
Hans-Christian Payer <hans@net-so.org>
2020-12-10 11:43:14 +0100

    uikit basics

---
+++ frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
+import { LeftNavigationComponent } from './left-navigation/left-navigation.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [TopNavigationComponent, LeftNavigationComponent, FooterComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    TopNavigationComponent,
+    LeftNavigationComponent,
+    FooterComponent
+  ]
+})
+export class SharedModule { }
